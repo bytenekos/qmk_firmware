d909626898360a28efb9b5d785b31a8328c6d924
ByteNeko <66184829+bytenekos@users.noreply.github.com>
2024-09-27 16:49:02 +0300

    Move some keys around and add arrow keys to hjkl

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/ByteNeko/keymap.c
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/ByteNeko/keymap.c
@@ -39,8 +39,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_FL] = LAYOUT_65_ansi_blocker(
   KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,    _______,  KC_INS,
-  _______,  KC_BTN1,  KC_MS_U,  KC_BTN2,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SCRL,  KC_PAUS,   _______,  _______,
-  _______,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_WH_D,  KC_WH_U,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  _______,             _______,
+  _______,  KC_BTN1,  KC_MS_U,  KC_BTN2,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_PSCR,  KC_SCRL,  KC_PAUS,   _______,  _______,
+  _______,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_WH_D,  KC_WH_U,  KC_LEFT,  KC_DOWN,  KC_UP,  KC_RGHT,  _______,  _______,  _______,             _______,
   _______,  RGB_HUI,  RGB_HUD,  RGB_SPD,  RGB_SPI,  _______,  _______,  _______,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,             RGB_VAI,  KC_HOME,
   _______,  _______,  _______,                                QK_BOOT,                                _______,  _______,  RGB_RMOD,  RGB_VAD,  RGB_MOD
   )
@@ -52,10 +52,8 @@ void keyboard_post_init_user(void) {
   rgblight_sethsv_noeeprom(0, 0, 255);
 }
 
-// Define the custom key binding
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (keycode == KC_ESC && (record->event.pressed && (get_mods() & (MOD_BIT(KC_LWIN) | MOD_BIT(KC_RWIN))))) {
-        // Press backtick key when Escape is pressed while holding Windows/Super key
+    if (keycode == KC_ESC && (record->event.pressed && (get_mods() & (MOD_BIT(KC_LWIN))))) {
         tap_code(KC_GRAVE);
         return false;
     }
